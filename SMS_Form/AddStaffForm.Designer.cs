5099f6a69e139f3ea897d2cd7512eefa7f87086c
shesanth2005 <uthayachandran0912@gmail.com>
2025-06-16 09:18:54 +0530

    16/06

--- SMS_Form/AddStaffForm.Designer.cs
+++ SMS_Form/AddStaffForm.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(228, 173);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // AddStaffForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_add);
+            this.Name = "AddStaffForm";
             this.Text = "AddStaffForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_add;
     }
 }
--- SMS_Form/AddStaffForm.cs
+++ SMS_Form/AddStaffForm.cs
@@ -12,9 +12,15 @@ namespace SMS_Form
 {
     public partial class AddStaffForm : Form
     {
+        public int UserId { get; set; }
         public AddStaffForm()
         {
             InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
